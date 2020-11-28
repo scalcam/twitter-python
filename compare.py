a503baa4cbf0811cddbb6f0c3b3e05fe2e00b908
Spencer Calcamuggio <scalcamuggio77@gmail.com>
2020-11-27 20:42:32 -0500

    changed log message

--- compare.py
+++ compare.py
@@ -6,11 +6,12 @@ def compare():
     with open('followers.json') as current:
         data_current = json.load(current)
 
+
     for follower in data_old['followers']:
         if follower in data_current['followers']:
-            print("follower in old list: ", follower['id'])
+            print("user still following: ", follower['id'])
         else:
-            print("**missing follower**: ", follower["id"])
+            print("**missing follower**: ", follower["id"], " name:", follower['name'], " @:", follower["screen_name"])
 
 def main():
     compare()
